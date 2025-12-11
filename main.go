0c8fdf101e9314bfba67823406b24ac1233c437f
Yulian302 <bohomolyulian3022003@gmail.com>
2025-12-11 21:52:13 +0100

    dev: add telemetry tracing for internal communications

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
+	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 
@@ -16,13 +19,29 @@ var (
 )
 
 func main() {
+	cfg := common.LoadConfig()
 	r := gin.Default()
+
+	// tracing
+	if cfg.Tracing {
+		tp, err := common.StartTracing()
+		if err != nil {
+			log.Fatalf("failed to start tracing: %v", err)
+		}
+		r.Use(otelgin.Middleware("gateway"))
+		defer func() { _ = tp.Shutdown(context.Background()) }()
+	}
 
 	r.GET("/test", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
 			"message": "ok",
 		})
 	})
+
+	// tel
+	clientHandler := otelgrpc.NewClientHandler(
+		otelgrpc.WithMessageEvents(otelgrpc.ReceivedEvents), // Record message events
+	)
 
 	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithStatsHandler(clientHandler))
 	if err != nil {
